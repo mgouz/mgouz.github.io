7d1b38b9ede9ac48f1baa63165b5c6335c099f77
Matthew Gouzoulis <mattgouzoulis@gmail.com>
2025-05-05 19:15:36 -0400

    Update personal site a bit

--- build-site.el
+++ build-site.el
@@ -27,7 +27,7 @@
 	     :publishing-function 'org-html-publish-to-html
 	     :with-author nil
 	     :with-creator t
-	     :with-toc t
+	     :with-toc nil
 	     :section-numbers nil
 	     :time-stamp-file nil)))
 
